1d9103bd2162d40d0bfd5a11b1a3c02dd97424e3
Deepankar Kotnala <dkotnala5@gmail.com>
2023-04-27 08:06:13 +0530

    Update basic_fastAPI_app.py

--- basic_fastAPI_app.py
+++ basic_fastAPI_app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
-
+from random import randrange
 import uvicorn
 
 app = FastAPI()
@@ -14,6 +14,10 @@ pydantic model used for Validation
 When we create a new post, this is stored in a pydantic model. 
 Pydantic model has a method called .dict which will convert the data into a dictionary. 
 '''
+## Instead of having a database, we will store the data in men=mory (for now)
+my_posts = [{"title": "title of post 1", "content":"content of post 1", "id": 1}, 
+            {"title": "favourite foods", "content":"I like pizza", "id": 2}]
+
 
 class Post(BaseModel):
     title: str
@@ -25,12 +29,18 @@ class Post(BaseModel):
 async def root():
     return {"message": "Hello World"}
 
+
+@app.get("/posts")
+def get_posts():
+    return {"data":my_posts}
+
+
 @app.post("/posts")
 def create_posts(post: Post):
-    # print(post.title)
-    # print(post.content)
-    print(post.dict())
-    return {"data":post}
+    post_dict = post.dict()     ## because we will get a pydantic array -- due to pydantic validation -- hence converting it to dictionary 
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data":post_dict}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
